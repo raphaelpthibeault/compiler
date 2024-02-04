78885a1c6b25f51dc52752708183379ea9c4eb8e
rpthibeault <rpthibeault@outlook.com>
2024-02-04 16:23:31 -0500

    added build to gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rpthibeault/Documents/compiler
-# Build directory: /home/rpthibeault/Documents/compiler/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
